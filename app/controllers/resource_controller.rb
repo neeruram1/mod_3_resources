d08d33a81ca37db390762344f8b3532612ff47ca
“Neeru <neeram85@gmail.com>
2020-08-21 17:39:31 -0600

    Create Resource controller

---
+++ app/controllers/resource_controller.rb
@@ -0,0 +1,7 @@
+class ResourceController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'resource/index'
+  get 'resource/show'
   root 'welcome#index'
 end
